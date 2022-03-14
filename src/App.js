bcd20030cad9e782fc703e256e3d159f9965246d
Bartosz Godek <Barutosz007@wp.pl>
2022-03-14 20:25:26 +0100

    checkout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
     <div className="App">
 
     <p>
+<<<<<<< HEAD
       
+=======
+      Paragraf Bartka.
+      Siema, spróbuje teraz bez tokena zpushować.
+>>>>>>> parent of 016d0b7 (zmiana 1)
     </p>
 
     </div>
