85b1449688c44070e57841c9bd80491e1444c5cb
Bartosz Godek <Barutosz007@wp.pl>
2022-03-14 20:45:03 +0100

    Fix merge2.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ function App() {
     <div className="App">
 
     <p>
-<<<<<<< HEAD
-      
-=======
       Paragraf Bartka.
       Siema, spróbuje teraz bez tokena zpushować.
->>>>>>> parent of 016d0b7 (zmiana 1)
     </p>
 
     </div>
