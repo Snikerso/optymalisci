a066eba7f8fb45494305f4707185487122cfdfd4
Snikerso <42245963+Snikerso@users.noreply.github.com>
2022-03-21 19:56:52 +0100

    Dodanie react router dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
+import {BrowserRouter,Routes, Route} from "react-router-dom"
+import {SnikersView} from "./views/SnikersView"
+
 
 function App() {
+  
+
   return (
     <>
-    <div className="App">
-
-    <p>
-      Paragraf Bartka.
-      Siema, spróbuje teraz bez tokena zpushować.   eeeeee
-      <span>pawełdadsada</span>
-    </p>
-
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<div>Home</div>} />
+            <Route path='bartus' element={<div>Bartus</div>} />
+            <Route path='kanonashi' element={<div>Kanonashi</div>} />
+            <Route path='snikers' element={<SnikersView />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
