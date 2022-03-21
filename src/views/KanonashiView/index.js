dc672855f7ddf4e9ee8b27f3b0941bdfbcbc921b
Kanonashi <100380552+Kanonashi@users.noreply.github.com>
2022-03-21 20:51:44 +0100

    nowa zmiana
    nowa zmiana

--- src/views/KanonashiView/index.js
+++ src/views/KanonashiView/index.js
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
+
 export const KanonashiView = () => {
-  return <div>Moj widok Kanonashi</div>;
+  return <div>Moj widok Kanonashi
+
+  <Link to={"/"}Back to home ></Link>
+
+</div>;
 };
