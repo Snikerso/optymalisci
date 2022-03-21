604fe67ef033c894cebff1e33d9e2d39f81e57af
Bartosz Godek <Barutosz007@wp.pl>
2022-03-21 20:53:59 +0100

    Update BartusView

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Link  to="/snikers">Link</Link>
+          <Link to="/bartus">Tutaj do bartusia!</Link>
           <Routes>
             <Route index element={<div>Home</div>} />
             <Route path='bartus' element={<BartusView />} />
--- src/views/BartusView/index.js
+++ src/views/BartusView/index.js
@@ -1,3 +1,8 @@
+import { Link } from "react-router-dom";
+
 export const BartusView = () => {
-    return <div> localhost:3000/bartus </div>
+    return <div> localhost:3000/bartus
+    
+    <Link to={"/"}> Back to home </Link> 
+    </div>
 }
