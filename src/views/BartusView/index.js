a3ac57d040b1c1cac81a07a71de25b3a9a30e708
Bartosz Godek <Barutosz007@wp.pl>
2022-04-01 19:55:23 +0200

    Update backgrounda

--- src/views/BartusView/index.js
+++ src/views/BartusView/index.js
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 
 export const BartusView = ({back, title}) => {
     return <div className='bartusDiv'>
-    <h1>{title}</h1>
-    <Link to={back.toSnikers}>Back to {back.textSnikers}</Link><br></br>
-    <Link to={back.toKanonashi}>Back to {back.textKanonashi}</Link><br></br>
-    <Link to={back.toHome}>Back to {back.textHome}</Link><br></br>
+    <div className='bartusBackground'></div>
+    <h1 className='bartusTitle'>{title}</h1>
+    <Link to={back.toSnikers} className={'bartusLink'}>Back to {back.textSnikers}</Link><br></br>
+    <Link to={back.toKanonashi} className={'bartusLink'}>Back to {back.textKanonashi}</Link><br></br>
+    <Link to={back.toHome} className={'bartusLink'}>Back to {back.textHome}</Link><br></br>
     </div>
 };
