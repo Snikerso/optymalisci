7857b8a7251e6d02663a2812237aa2d0b02af1a1
Bartosz Godek <Barutosz007@wp.pl>
2022-04-02 17:50:44 +0200

    porzadek

--- src/views/BartusView/index.js
+++ src/views/BartusView/index.js
@@ -5,8 +5,6 @@ export const BartusView = ({back, title}) => {
     return <div className='bartusDiv'>
     <div className='bartusBackground'></div>
     <h1 className='bartusTitle'>{title}</h1>
-    <Link to={back.toSnikers} className={'bartusLink'}>Back to {back.textSnikers}</Link><br></br>
-    <Link to={back.toKanonashi} className={'bartusLink'}>Back to {back.textKanonashi}</Link><br></br>
     <Link to={back.toHome} className={'bartusLink'}>Back to {back.textHome}</Link><br></br>
     </div>
 };
