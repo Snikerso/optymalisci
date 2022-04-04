32876cd86372d2c53e22de850ecc241835a109b3
GniewomirX <102823829+GniewomirX@users.noreply.github.com>
2022-04-04 20:06:49 +0200

    atom licznika

--- src/views/GniewomirXView/index.js
+++ src/views/GniewomirXView/index.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import React from "react"
+import { Counter } from "../../components/atoms/Counter";
 export const GniewomirXView = ({back, title}) => {
 
   return <div>
+    <Counter>0</Counter>
     <h1>{title}</h1>
     <Link to={back.to}>{back.text}</Link>
   </div>
