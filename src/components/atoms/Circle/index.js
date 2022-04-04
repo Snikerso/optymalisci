d29ff00dca181b5b49f7fb67a39565c6c8cd30c1
Kanonashi <100380552+Kanonashi@users.noreply.github.com>
2022-04-04 21:01:38 +0200

    moje koło

---
+++ src/components/atoms/Circle/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Circle = styled.div`
+width: 50px;
+height: 50px;
+background-color: pink;
+margin: 50px auto; // 50px wartość góra dół
+border-radius: 50%;
+border: 2px solid red;
+`
--- src/views/KanonashiView/index.js
+++ src/views/KanonashiView/index.js
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import { Counter } from "../../components/atoms/Counter";
+import { Circle } from '../../components/atoms/Circle';
 export const KanonashiView = ({ title, back }) => {
   return (
     <div>
+      {/* Tu będzie guzik */}
+      <Circle onScroll={() => console.log(`konntroluje scrolla`)}>
+      
+      </Circle>
       <div className="background"></div>
       <h1>{title} Witaj na mojej podstronie</h1>
       <h2>Słodkiego dnia życze</h2>
