c49e39133c2b866fd9ec9bc04fb2dd5f7ad9f662
Snikerso <42245963+Snikerso@users.noreply.github.com>
2022-04-25 19:31:45 +0200

    input title

--- src/views/HomeView/index.js
+++ src/views/HomeView/index.js
@@ -2,40 +2,34 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const HomeView = ({ optymalists }) => {
-
-  // const [typeSort, setTypeSort] = useState('Ascending');
-  // let newOp = optymalists;
-  //
-  // newOp = newOp.sort((a, b) => {
-  //   if (typeSort === 'Ascending') {
-  //     if (a.title > b.title) return -1;
-  //   } else {
-  //     if (a.title < b.title) return -1;
-  //   }
-  // });
-
-    const [title, setTitle] = useState('')
-    console.log(title);
-
+  const [inputTitle, setInputTitle] = useState('');
 
   return (
     <div>
       <h1>Home PAage</h1>
-      {/*<button*/}
-      {/*  onClick={() => setTypeSort((prev) => (prev === 'Ascending' ? 'Descending' : 'Ascending'))}>*/}
-      {/*  {typeSort}*/}
-      {/*</button>*/}
 
-        <input type='text' onChange={(event) => {setTitle(event.target.value)}}
-               />
+      <input
+        type="text"
+        onClick
+        onChange={(event) => {
+          setInputTitle(event.target.value);
+        }}
+      />
 
-      {optymalists.filter((item) => {
-        if (item.title.toLowerCase().includes(title)) {
-          return item;
-        }
-      }).map(item => <><Link to={item.to} key={item.id}> {item.title} </Link> <br/></>)
+      {optymalists
+        .filter((item) => {
+          if (item.title.toLowerCase().includes(inputTitle)) {
+            return item;
           }
-
+        })
+        .map((item) => (
+          <>
+            <Link to={item.to} key={item.id}>
+              {item.title.toLowerCase()}
+            </Link>
+            <br />
+          </>
+        ))}
     </div>
   );
 };
