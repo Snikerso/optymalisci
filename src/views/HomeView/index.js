563d941673f21bf39f0b3f3f77e8ded2b85b53ce
GniewomirX <102823829+GniewomirX@users.noreply.github.com>
2022-04-25 21:00:41 +0200

    przyciskDodaj

--- src/views/HomeView/index.js
+++ src/views/HomeView/index.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import {Button} from '../../components/atoms/Button';
+
 
 export const HomeView = ({ optymalists }) => {
   const [inputTitle, setInputTitle] = useState('');
-
+  const [optymalistsArray, setOptymalistsArray] = useState(optymalists)
+  //console.log(optymalistsArray)
   return (
     <div>
-      <h1>Home PAage</h1>
+      <h1>Home Page</h1>
 
       <input
         cos="ds"
@@ -16,9 +19,19 @@ export const HomeView = ({ optymalists }) => {
         }}
       />
 
-      {optymalists
+      <Button
+        onClick={() => setOptymalistsArray(  [...optymalistsArray,{
+          id: optymalistsArray.length+1,
+          to: `/${inputTitle}`,
+          text: 'Do'+ inputTitle,
+          title: inputTitle,
+          back: { toHome: '/', textHome: 'Home' }
+        }])}>Dodaj</Button>
+
+      {optymalistsArray
         .filter((item) => {
-          if (item.title.toLowerCase().includes(inputTitle)) {
+          //console.log(item.title.toLowerCase().includes(inputTitle),inputTitle)
+          if (item.title.toLowerCase().includes(inputTitle.toLowerCase())) {
             return item;
           }
         })
